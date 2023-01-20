b56e9dc9ada29aacda0be023f6d6e742108e16b1
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2023-01-20 10:47:14 +0100

    css et visu Rencontre

--- pagesPhp/visuRencontre.php
+++ pagesPhp/visuRencontre.php
@@ -4,9 +4,118 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/Rencontre.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    
+    <input type="button" value="Modifier">
+
+    <div class="LesPitiCarts">
+        <div class="pitiCarte">
+            <table>
+            <tr>
+                <th>
+                    Nom equipe adverse
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    unNom
+                </td>
+            </tr>
+            </table>
+        </div>
+        <div class="pitiCarte">
+            <table>
+            <tr>
+                <th>
+                    Heure debut
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    uneHeuere
+                </td>
+            </tr>
+            </table>
+        </div>
+        <div class="pitiCarte">
+            <table>
+            <tr>
+                <th>
+                    Heure fin
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    uneHeuere
+                </td>
+            </tr>
+            </table>
+        </div>
+        <div class="pitiCarte">
+            <table>
+            <tr>
+                <th>
+                    Date
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    uneDate
+                </td>
+            </tr>
+            </table>
+        </div>
+        <div class="pitiCarte">
+            <table>
+            <tr>
+                <th>
+                    Lieu
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    unLieu
+                </td>
+            </tr>
+            </table>
+        </div>
+        <div class="pitiCarte">
+            <table>
+            <tr>
+                <th>
+                    Score locaux
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    unScore
+                </td>
+            </tr>
+            </table>
+        </div>
+        <div class="pitiCarte">
+            <table>
+            <tr>
+                <th>
+                    Score Visiteur
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    unScore
+                </td>
+            </tr>
+            </table>
+        </div>
+    </div> 
+
+
+    <div class="laListe">
+        liste joueur du match <br>
+
+        gfnh;gjhkgjhfvcqnsgxhf;cgjk:hvsdbwvfx hckDYkg
+    </div>
 </body>
 </html>
