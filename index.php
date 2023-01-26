f53a753c4244184198fe341c31517e3215a857cc
Benoit <benoit.leclerc@etu.iut-tlse3.fr>
2023-01-26 15:05:26 +0100

    css

--- index.php
+++ index.php
@@ -34,6 +34,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="./css/login.css" rel="stylesheet">
 	<title>Connectez vous !</title>
 </head>
 <body>
